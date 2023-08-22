7fb2f72f2e827ec5595c522212d83842ebc947b1
YSG <dbtjrrbs12@naver.com>
2023-08-22 23:08:04 +0900

    init

---
+++ src/main/java/everyCamp/campback/CampbackApplication.java
@@ -0,0 +1,13 @@
+package everyCamp.campback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/everyCamp/campback/CampbackApplicationTests.java
@@ -0,0 +1,13 @@
+package everyCamp.campback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

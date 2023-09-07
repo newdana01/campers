de96ab5acfaecc6b8e68bee9b9ba034c53a148db
newdana01 <newdana01@gmail.com>
2023-09-07 15:39:48 +0900

    feat: TeamMember Builder 추가

--- src/main/java/everyCamp/campback/team/entity/TeamMember.java
+++ src/main/java/everyCamp/campback/team/entity/TeamMember.java
@@ -1,8 +1,11 @@
 package everyCamp.campback.team.entity;
 
 import everyCamp.campback.common.entity.BaseEntity;
+import everyCamp.campback.common.entity.PreferRegion;
+import everyCamp.campback.common.entity.PreferType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
@@ -11,6 +14,7 @@ import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "team_members")
@@ -32,4 +36,14 @@ public class TeamMember{
     private LocalDateTime joinDt;
     @Column(name = "exit_dt_or_null")
     private LocalDateTime exitDtOrNull;
+
+    @Builder
+    protected TeamMember(
+            Team team,
+            User user,
+            int recruitNumber
+    ) {
+        this.team = team;
+        this.user = user;
+    }
 }

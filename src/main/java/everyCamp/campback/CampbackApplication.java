6a0f986fe1effc82ecd58f5d2219f428c26439a6
newdana01 <newdana01@gmail.com>
2023-09-07 16:35:41 +0900

    feat: @EnableJpaAuditing 어노테이션 추가
    createdDt, updatedDt 컬럽 업데이트 자동화를 위한 어노테이션 추가

--- src/main/java/everyCamp/campback/CampbackApplication.java
+++ src/main/java/everyCamp/campback/CampbackApplication.java
@@ -2,7 +2,9 @@ package everyCamp.campback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CampbackApplication {
 

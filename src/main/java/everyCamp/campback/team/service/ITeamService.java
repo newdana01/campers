8880d760978adf1f04b86b7c2505e2c8623703fd
newdana01 <newdana01@gmail.com>
2023-09-11 15:31:44 +0900

    Feat: ITeamService 추상 메서드 정의
    - createTeam(), findTeam(), updateTeam(), deleteTeam() 추가

---
+++ src/main/java/everyCamp/campback/team/service/ITeamService.java
@@ -0,0 +1,15 @@
+package everyCamp.campback.team.service;
+
+import everyCamp.campback.team.dto.TeamCreateDto;
+import everyCamp.campback.team.dto.TeamResponse;
+import everyCamp.campback.team.dto.TeamUpdateDto;
+import org.springframework.stereotype.Service;
+
+public interface ITeamService {
+    TeamResponse createTeam(TeamCreateDto team);
+    TeamResponse findTeam(String teamId);
+
+    TeamResponse updateTeam(TeamUpdateDto team);
+
+    void deleteTeam(String teamId);
+}

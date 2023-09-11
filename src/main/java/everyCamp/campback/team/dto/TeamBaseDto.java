e846d818f4adc23e71afca301f18206f5e807176
newdana01 <newdana01@gmail.com>
2023-09-11 15:27:45 +0900

    feat: TeamBaseDto 수정
    - 필드명 변경 (name -> teamName)
    - 어노테이션 추가 (@SuperBuilder, @Getter, @NoArgsConstructor)

--- src/main/java/everyCamp/campback/team/dto/TeamBaseDto.java
+++ src/main/java/everyCamp/campback/team/dto/TeamBaseDto.java
@@ -2,12 +2,18 @@ package everyCamp.campback.team.dto;
 
 import everyCamp.campback.common.entity.PreferRegion;
 import everyCamp.campback.common.entity.PreferType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@SuperBuilder
+@Getter
+@NoArgsConstructor
 public abstract class TeamBaseDto {
-    private String name;
+    private String teamName;
     private int recruitNumber;
     private List<PreferRegion> preferRegions = new ArrayList<>();
     private List<PreferType> preferTypes = new ArrayList<>();

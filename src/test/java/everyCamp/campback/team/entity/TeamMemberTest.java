fa8d69a741aa8434c51aa1cb7d853786f6429713
newdana01 <newdana01@gmail.com>
2023-09-12 11:14:24 +0900

    Feat: TeamMemberTest 클래스 생성
    - exitTeamTest() 추가

---
+++ src/test/java/everyCamp/campback/team/entity/TeamMemberTest.java
@@ -0,0 +1,23 @@
+package everyCamp.campback.team.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TeamMemberTest {
+    @Test
+    @DisplayName("팀을 추가한다")
+    void exitTeamTest() {
+        //given
+        Team team = Team.builder().name("team1").build();
+        TeamMember teamMember = TeamMember.builder().build();
+        //when
+        teamMember.setTeam(team);
+        //then
+        assertThat(team.getTeamMembers().size()).isEqualTo(1);
+        assertThat(teamMember.getTeam().getName()).isEqualTo("team1");
+        assertThat(team.getTeamMembers().get(0).getId()).isEqualTo(teamMember.getId());
+    }
+}

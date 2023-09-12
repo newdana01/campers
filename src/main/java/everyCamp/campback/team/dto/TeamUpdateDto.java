7e4061a2e059c178b0011a8ea7d132cb8742fa86
newdana01 <newdana01@gmail.com>
2023-09-12 15:09:46 +0900

    Feat: TeamUpdateDto 빌더추가

--- src/main/java/everyCamp/campback/team/dto/TeamUpdateDto.java
+++ src/main/java/everyCamp/campback/team/dto/TeamUpdateDto.java
@@ -2,10 +2,12 @@ package everyCamp.campback.team.dto;
 
 import everyCamp.campback.common.entity.PreferRegion;
 import everyCamp.campback.common.entity.PreferType;
+import lombok.Builder;
 import lombok.Getter;
 import java.util.Set;
 
 @Getter
+@Builder
 public class TeamUpdateDto {
     private String teamName;
     private Set<PreferRegion> preferRegions;

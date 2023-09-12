b337bc41b7f95148fb7c6a7e1bb1851307ed6528
newdana01 <newdana01@gmail.com>
2023-09-12 11:03:31 +0900

    Feat: TeamTest addPreferRegionTest() 추가

--- src/test/java/everyCamp/campback/team/entity/TeamTest.java
+++ src/test/java/everyCamp/campback/team/entity/TeamTest.java
@@ -1,10 +1,14 @@
 package everyCamp.campback.team.entity;
 
+import everyCamp.campback.common.entity.PreferRegion;
+import everyCamp.campback.common.entity.PreferType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class TeamTest {
     @Test
@@ -13,7 +17,18 @@ public class TeamTest {
         LocalDateTime now = LocalDateTime.now();
         Team team = Team.builder().name("team").build();
         team.deleteTeam();
-        Assertions.assertThat(team.getDeletedDtOrNull()).isNotNull();
-        Assertions.assertThat(team.getDeletedDtOrNull()).isAfter(now);
+        assertThat(team.getDeletedDtOrNull()).isNotNull();
+        assertThat(team.getDeletedDtOrNull()).isAfter(now);
+    }
+
+    @Test
+    @DisplayName("선호지역을 추가한다")
+    void addPreferRegionTest() {
+        Team team = Team.builder().name("team").build();
+        PreferRegion preferRegion = PreferRegion.builder().name("서울").build();
+        team.addPreferRegion(preferRegion);
+        PreferRegion p = team.getPreferRegions().get(0);
+        assertThat(team.getPreferRegions().size()).isEqualTo(1);
+        assertThat(p.getName()).isEqualTo("서울");
     }
 }

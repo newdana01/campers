ff5a5e52daef1fdd7ecbe42add96f474226cd385
newdana01 <newdana01@gmail.com>
2023-09-13 17:42:17 +0900

    Feat: ITeamCustomRepository 클래스 추가

---
+++ src/main/java/everyCamp/campback/team/repository/ITeamCustomRepository.java
@@ -0,0 +1,9 @@
+package everyCamp.campback.team.repository;
+
+import everyCamp.campback.team.entity.Team;
+
+import java.util.List;
+
+public interface ITeamCustomRepository {
+    List<Team> findAllNotPostedTeams(String userId);
+}

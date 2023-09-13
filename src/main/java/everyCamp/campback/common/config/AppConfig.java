1fe07de5cc40852e11acff072d573d184ecb11e4
newdana01 <newdana01@gmail.com>
2023-09-13 17:39:01 +0900

    Feat: jpaQueryFactory 빈 설정 추가

--- src/main/java/everyCamp/campback/common/config/AppConfig.java
+++ src/main/java/everyCamp/campback/common/config/AppConfig.java
@@ -1,9 +1,22 @@
 package everyCamp.campback.common.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("everyCamp.campback")
+@RequiredArgsConstructor
 public class AppConfig {
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
 }

346c69316b99079f198ba795fac3269fa22ab01c
newdana01 <newdana01@gmail.com>
2023-09-13 21:11:41 +0900

    Feat: ITeamRepository 상속 관계 추가 (ITeamCustomRepository)

--- src/main/java/everyCamp/campback/team/repository/ITeamRepository.java
+++ src/main/java/everyCamp/campback/team/repository/ITeamRepository.java
@@ -3,5 +3,5 @@ package everyCamp.campback.team.repository;
 import everyCamp.campback.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ITeamRepository extends JpaRepository<Team, String> {
+public interface ITeamRepository extends JpaRepository<Team, String>, ITeamCustomRepository{
 }

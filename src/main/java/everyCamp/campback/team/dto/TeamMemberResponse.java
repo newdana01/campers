6d3f24feefd985cd29aae3699f883d4e9f91ed30
newdana01 <newdana01@gmail.com>
2023-09-13 21:37:10 +0900

    Feat: fetch type lazy 적용
    - TeamApplier, TeamMember 클래스에 성능을 위한 lazy 타입의 fetch 적용

---
+++ src/main/java/everyCamp/campback/team/dto/TeamMemberResponse.java
@@ -0,0 +1,29 @@
+package everyCamp.campback.team.dto;
+
+import everyCamp.campback.common.entity.PreferType;
+import everyCamp.campback.team.entity.Team;
+import everyCamp.campback.team.entity.TeamMember;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Builder
+public class TeamMemberResponse {
+    private String userId;
+    private String userName;
+    private String userNickname;
+    private String userProfileImageUrl;
+    private Set<PreferType> preferTypes;
+    private LocalDateTime joinDt;
+
+    public static TeamMemberResponse from(TeamMember teamMember) {
+        return TeamMemberResponse.builder()
+                .userId(teamMember.getUser().getName())
+                .userNickname(teamMember.getUser().getNickName())
+                .joinDt(teamMember.getJoinDt())
+                .build();
+    }
+}

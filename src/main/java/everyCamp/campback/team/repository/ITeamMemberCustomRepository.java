4410aaea6410ad20cd23d3bd45f3e328c0f7f509
newdana01 <newdana01@gmail.com>
2023-09-14 11:54:47 +0900

    Feat: TeamMember repository findTeamMembers()추상 메서드 추가

--- src/main/java/everyCamp/campback/team/repository/ITeamMemberCustomRepository.java
+++ src/main/java/everyCamp/campback/team/repository/ITeamMemberCustomRepository.java
@@ -1,4 +1,9 @@
 package everyCamp.campback.team.repository;
 
+import everyCamp.campback.team.entity.TeamMember;
+
+import java.util.List;
+
 public interface ITeamMemberCustomRepository {
+    List<TeamMember> findTeamMembers(String teamId);
 }

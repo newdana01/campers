c09e39bdd339283c3a07bdce07900c6402fd7ff8
newdana01 <newdana01@gmail.com>
2023-09-14 11:56:17 +0900

    Feat: TeamMemberRepositoryImpl 구현
    - findTeamMembers() 구현

--- src/main/java/everyCamp/campback/team/repository/TeamMemberRepositoryImpl.java
+++ src/main/java/everyCamp/campback/team/repository/TeamMemberRepositoryImpl.java
@@ -1,4 +1,25 @@
 package everyCamp.campback.team.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import everyCamp.campback.team.entity.TeamMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static everyCamp.campback.team.entity.QTeamMember.teamMember;
+
+@Repository
+@RequiredArgsConstructor
 public class TeamMemberRepositoryImpl implements ITeamMemberCustomRepository{
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<TeamMember> findTeamMembers(String teamId) {
+        return jpaQueryFactory
+                .selectFrom(teamMember)
+                .leftJoin(teamMember.user).fetchJoin()
+                .fetch();
+    }
+
 }

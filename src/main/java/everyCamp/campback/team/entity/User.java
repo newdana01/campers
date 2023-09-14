787ed765bd7664a2b0100b686bebdca2cefd1674
newdana01 <newdana01@gmail.com>
2023-09-14 11:55:08 +0900

    Feat: 더미 유저 변경

--- src/main/java/everyCamp/campback/team/entity/User.java
+++ src/main/java/everyCamp/campback/team/entity/User.java
@@ -1,12 +1,25 @@
 package everyCamp.campback.team.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "users")
 public class User {
     @Id
     @Column(name = "user_id")
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     private String id;
+    private String name;
+    private String nickname;
+    private String profileImageUrl;
 }

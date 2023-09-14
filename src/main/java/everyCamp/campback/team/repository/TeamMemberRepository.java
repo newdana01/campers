a3a1ad7bdfb6116898d40711646ff7880e75f0e9
newdana01 <newdana01@gmail.com>
2023-09-14 11:52:51 +0900

    Feat: querydsl 명명 규칙 준수를 위한 레파지터리 인터페이스명 변경

--- src/main/java/everyCamp/campback/team/repository/TeamMemberRepository.java
+++ src/main/java/everyCamp/campback/team/repository/TeamMemberRepository.java
@@ -3,5 +3,5 @@ package everyCamp.campback.team.repository;
 import everyCamp.campback.team.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ITeamMemberRepository extends JpaRepository<TeamMember, String>, ITeamMemberCustomRepository{
+public interface TeamMemberRepository extends JpaRepository<TeamMember, String>, ITeamMemberCustomRepository{
 }

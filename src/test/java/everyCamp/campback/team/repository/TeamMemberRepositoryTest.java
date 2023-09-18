3554294d5eeebf26d947ed82d27f31f5ebfccea7
newdana01 <newdana01@gmail.com>
2023-09-18 14:56:22 +0900

    Feat: 테스트 프로파일 구동 설정 추가

--- src/test/java/everyCamp/campback/team/repository/TeamMemberRepositoryTest.java
+++ src/test/java/everyCamp/campback/team/repository/TeamMemberRepositoryTest.java
@@ -8,14 +8,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 @Transactional
+@SpringBootTest
+@ActiveProfiles("test")
 public class TeamMemberRepositoryTest {
     @Autowired
     private TeamMemberRepository teamMemberRepository;

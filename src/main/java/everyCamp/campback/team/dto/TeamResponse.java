853d933813c9b64bcae310bbd1aed78c0b6b36e4
newdana01 <newdana01@gmail.com>
2023-09-19 18:23:16 +0900

    Feat: from() 수정
    - TeamMemeber엔티티를 TeamMemberResponse dto로 변환과정 추가

--- src/main/java/everyCamp/campback/team/dto/TeamResponse.java
+++ src/main/java/everyCamp/campback/team/dto/TeamResponse.java
@@ -2,33 +2,41 @@ package everyCamp.campback.team.dto;
 
 
 import everyCamp.campback.team.entity.Team;
-import everyCamp.campback.user.entity.User;
+import everyCamp.campback.team.entity.TeamMember;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @RequiredArgsConstructor
 @SuperBuilder
 public class TeamResponse extends TeamBaseDto {
     private String teamId;
-    private User leader;
+    private String leaderId;
     private String intro;
     private LocalDateTime createdDt;
+    private Set<TeamMemberResponse> members;
 
     public static TeamResponse from(Team team) {
+        Set<TeamMemberResponse> memberRes = new HashSet<>();
+        for (TeamMember teamMember : team.getTeamMembers()) {
+            memberRes.add(TeamMemberResponse.from(teamMember));
+        }
+
         return TeamResponse.builder()
                 .teamId(team.getId())
                 .teamName(team.getName())
                 .recruitNumber(team.getRecruitNumber())
-                .leader(team.getLeader())
+                .leaderId(team.getLeader().getId())
                 .preferTypes(team.getPreferTypes())
                 .preferRegions(team.getPreferRegions())
                 .intro(team.getIntro())
+                .createdDt(team.getCreatedDt())
+                .members(memberRes)
                 .build();
-
     }
-
 }

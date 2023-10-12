c89d38c5aaaeb62a345ed6ab5c1e35f2d94a8560
newdana01 <newdana01@gmail.com>
2023-10-12 21:59:02 +0900

    Feat: TeamMemberServiceImpl - dropMember 권한 검사 주석 추가

--- src/main/java/everyCamp/campback/team/service/TeamMemberServiceImpl.java
+++ src/main/java/everyCamp/campback/team/service/TeamMemberServiceImpl.java
@@ -1,19 +1,23 @@
 package everyCamp.campback.team.service;
 
 import everyCamp.campback.team.dto.TeamMemberResponse;
+import everyCamp.campback.team.entity.Team;
 import everyCamp.campback.team.entity.TeamMember;
 import everyCamp.campback.team.repository.TeamMemberRepository;
+import everyCamp.campback.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamMemberServiceImpl implements ITeamMemberService{
     private final TeamMemberRepository teamMemberRepository;
+    private final TeamRepository teamRepository;
 
     @Override
     public List<TeamMemberResponse> findAllMembers(String teamId) {
@@ -24,6 +28,11 @@ public class TeamMemberServiceImpl implements ITeamMemberService{
     @Override
     @Transactional
     public void dropMember(String teamId, String userId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(NoSuchElementException::new);
+//        if (team.getLeader().getId().equals(userId)) {
+//            // 권한 에러 추가
+//        }
+
         TeamMember teamMember = teamMemberRepository.findOneByTeamIdUserId(teamId, userId);
         teamMember.exitTeam();
     }

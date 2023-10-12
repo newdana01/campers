bdb3f1339c7a8f51415aad589b759d3bdbb03f29
newdana01 <newdana01@gmail.com>
2023-10-12 21:58:17 +0900

    Feat: findAllMembers, dropMember() 추가

--- src/main/java/everyCamp/campback/team/api/TeamMemberApiController.java
+++ src/main/java/everyCamp/campback/team/api/TeamMemberApiController.java
@@ -1,9 +1,30 @@
 package everyCamp.campback.team.api;
 
+import everyCamp.campback.team.dto.TeamMemberResponse;
+import everyCamp.campback.team.service.ITeamMemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TeamMemberApiController extends TeamMemberBaseController{
+    private final ITeamMemberService teamMemberService;
+    @GetMapping("")
+    public List<TeamMemberResponse> findAllMembers(@RequestParam("team_id") String teamId) {
+        return teamMemberService.findAllMembers(teamId);
+    }
+
+    @PatchMapping("")
+    public void dropMember(@RequestParam("team_id") String teamId, @RequestBody DropMemberDto dropMemberDto){
+        // TODO: 팀장 권한 검사 추가
+        teamMemberService.dropMember(teamId, dropMemberDto.getUserId());
+    }
+
+    @Getter
+    static class DropMemberDto{
+        private String userId;
+    }
 }
